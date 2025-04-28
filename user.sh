3d2ff76e61500a8da8bc63676434b5e218bfeab0
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-28 12:29:56 +0530

    condition

--- user.sh
+++ user.sh
@@ -5,6 +5,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+MONGO_HOST=mongodb.devopswithmsvs.uno
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -23,4 +25,55 @@ then
 else
     echo -e "$G you are a root user $N"
 fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "disabling nodejs successfull"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "enabling nodejs:18 successfull"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+else  
+    echo -e "$G roboshop user already exists $N"
+fi
+    
+VALIDATE $? "userroboshop added"
+
+mkdir -p /app 
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+VALIDATE $? "downloading user application"
+
+cd /app 
+
+unzip /tmp/user.zip &>> $LOGFILE
+VALIDATE $? "UNZIPPING user"
+
+npm install &>> $LOGFILE
+VALIDATE $? "dependencies created"
+
+cp user.service /etc/systemd/system/user.service &>> $LOGFILE
+VALIDATE $? "copying user.service"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "deamon reloaded successfully"
+
+systemctl enable user &>> $LOGFILE
+VALIDATE $? "user enabled successfully"
+
+systemctl start user &>> $LOGFILE
+VALIDATE $? "system started successfully"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copied mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "installed mongodb client successfully"
+
+mongo --host $MONGO_HOST </app/schema/user.js &>> $LOGFILE
+VALIDATE $? "loading user data into mongodb"
 
